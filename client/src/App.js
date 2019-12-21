ffe1ce7a06780837d81b28050c4e481ccaf76188
alperkagankayali <alper.kagan.kayali@gmail.com>
2019-12-21 16:40:45 +0300

    More modification on UI

--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,22 @@ import logo from './logo.svg';
 import  { Link, Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
 
-import Home from './components/Hotel.js';
+
+import Home from './components/Home.js';
+import Listing from './components/Listing.js';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div>
         <Router>
-          <div>
-            <h2>Welcome to React Router Tutorial</h2>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              <li><Link to={'/home'} className="nav-link"> Home </Link></li>
-            </ul>
-            </nav>
-            <hr />
-            <Switch>
-              <Router>
-                <Route exact path='/home' component={Home} />
-              </Router>
-            </Switch>
-          </div>
+          {/* Public Routes */}
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/listing" component={Listing}/>
         </Router>
+          
+        </div>
         
       </header>
     </div>
---
+++ client/src/components/Listing.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import StarIcon from '@material-ui/icons/StarBorder';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles(theme => ({
+    '@global': {
+      ul: {
+        margin: 0,
+        padding: 0,
+      },
+      li: {
+        listStyle: 'none',
+      },
+    },
+    appBar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+      flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+      flexGrow: 1,
+    },
+    link: {
+      margin: theme.spacing(1, 1.5),
+    },
+    heroContent: {
+      padding: theme.spacing(8, 0, 6),
+    },
+    cardHeader: {
+      backgroundColor:
+        theme.palette.type === 'dark' ? theme.palette.grey[700] : theme.palette.grey[200],
+    },
+    cardPricing: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'baseline',
+      marginBottom: theme.spacing(2),
+    },
+    footer: {
+      borderTop: `1px solid ${theme.palette.divider}`,
+      marginTop: theme.spacing(8),
+      paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(3),
+      [theme.breakpoints.up('sm')]: {
+        paddingTop: theme.spacing(6),
+        paddingBottom: theme.spacing(6),
+      },
+    },
+}));
+  
+const tiers = [
+{
+    title: 'Anan',
+    path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS78aY9cQ2xD09zNdGL4ekYGHjLELKukyFaNRaH0wnHzCHF1U33',
+    buttonText: 'Sign up for free',
+    buttonVariant: 'contained',
+},
+{
+    title: 'Restaurant Review',
+    path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSEzFMbXyfPLTI_4YA2JCY71W_G-0Zfo7_ENAfnR3P4a6vMF39a',
+    buttonText: 'Get started',
+    buttonVariant: 'contained',
+},
+{
+    title: 'Movie Review',
+    path: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRQk-CnIRaVuro_ji6wCxX09yhVbRfTO_IMM0EQjw24VODGE9n3',
+    buttonText: 'Contact us',
+    buttonVariant: 'contained',
+},
+];
+export default function Pricing() {
+    const classes = useStyles();
+  
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        {/* Hero unit */}
+        <Container maxWidth="sm" component="main" className={classes.heroContent}>
+          <Typography component="h1" variant="h2" align="center" gutterBottom>
+            Review System
+          </Typography>
+        </Container>
+        {/* End hero unit */}
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+            {tiers.map(tier => (
+              // Enterprise card is full width at sm breakpoint
+              <Grid item key={tier.title} xs={12} sm={tier.title === 'Restaurant Review' ? 12 : 6} md={5}>
+                <Card>
+                  <CardHeader
+                    title={tier.title}
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    action={tier.title === 'Pro' ? <StarIcon /> : null}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <ul>
+                        <img src={tier.path} width="193" height="130"></img>
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button fullWidth variant={tier.buttonVariant} color="primary">
+                      {tier.buttonText}
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </React.Fragment>
+    );
+}
