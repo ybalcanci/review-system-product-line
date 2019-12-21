484e59be792a1f971786e1c77ef8e5901b217be3
alperkagankayali <alper.kagan.kayali@gmail.com>
2019-12-21 23:35:09 +0300

    app.js fixed

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Home from './components/Home'
-<<<<<<< Updated upstream
 
 import Review from './components/Entity'
 import Listing from './components/Listing'
 
-=======
-import Listing from './components/Listing'
-import Review from './components/Review'
->>>>>>> Stashed changes
 import ReviewPage from './ReviewPage'
 
 /***
